6bc1d60bf2a63cdfa7fa6190d3eae5174fa74704
Ricardo Guajardo <rguajardo0103@stu.neisd.net>
2015-09-30 06:05:14 -0500

    My datatype from ricardo Guajardo

---
+++ src/guajardo_3_datatypes/Guajardo_3_datatypes.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guajardo_3_datatypes;
+
+/**
+ *
+ * @author rguajardo0103
+ */
+public class Guajardo_3_datatypes {
+
+    public static void main(String[] args) {
+     //This a comment 
+     //We will uce many comment to explain are code
+     //This program explores some of the primative data types
+     int i = 0;
+     float a = 0.01f;
+     double b = 0.01;
+     short c =123;
+     char d1 = ':';
+     char d2 = 'a';
+     char d3 = 42;
+     char d4 = 145;
+     String e = d4+"";
+     String e1 = "Roader is the best football player";
+     boolean f = true;
+     boolean g = i % 2 == 0;
+     
+     
+     /*
+     here we will look at some mathematical operator
+     */
+     // + this adds to things and more 
+     // "-" this subtracts two numbers and more 
+     // "*" this multiplies two numbers and more
+     // "/"  rhis divides two numbers and more
+     // "%: this is modulo. shows the reminders
+     i=7;
+     a=0;
+     e = "ninja";
+     System.out.println(i + a + " an interger / "+ " plus a float");
+        System.out.println(e + i + " an/" + " a string plus an interger" );
+     System.out.println(a + b + " a float/" + " plus a doucle");
+     System.out.println(b + c + " a double/ " + "plus a short ");
+     System.out.println(c + " a short");
+     System.out.println(d1 + " a char ");
+     System.out.println(d2 + " a char ");
+     System.out.println(d3 + " a char ");
+     System.out.println(d4 + " a char ");
+     System.out.println(e + " a string");
+     System.out.println(f + " a boolean");
+     System.out.println(g + " a boolean");
+//        for(i = -123;i < 123; i++){
+//           // System.out.println((char) i);
+//            System.out.println(i % 3 == 0);
+//    }
+     // char shows a number value maps to a character
+    }
+    
+}
